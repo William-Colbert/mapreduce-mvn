f8b3a2964f581011de487275231ff8ab044f4f58
William Colbert <S544228@nwmissouri.edu>
2024-02-23 13:50:14 -0600

    Found creditCard havers

--- src/main/java/com/mycompany/app/MyMapper.java
+++ src/main/java/com/mycompany/app/MyMapper.java
@@ -19,7 +19,7 @@ public class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
         String[] row = value.toString().split("\t");
-        context.write(new Text(row[1] + "_" + row[2]), new IntWritable(1));
+        context.write(new Text(row[10]), new IntWritable(1));
     }
     
 }
